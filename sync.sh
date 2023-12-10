292f03e3b241d30f70f02403b09f8b77b963e687
Stefferson Moraes Teixera <smoraes-@cbr9s6.42barcelona.com>
2023-12-10 14:15:25 +0100

    tardir_sh

--- sync.sh
+++ sync.sh
@@ -5,6 +5,9 @@ read -p "Introduce commit: " coomit
 
 
 rsync -av --progress ~/Templates/Repos/* ~/git/42Piscine --exclude .git
+rsync -av --progress ~/.vimrc ~/git/.dotfiles/.vimrc
+rsync -av --progress ~/.bashrc ~/git/.dotfiles/.bashrc
+
 cd ~/git/42Piscine/
 
 git add .
