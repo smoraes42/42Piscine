625dfed40f2fc36e80a9a12c411f7cc7f3a0ac65
Stefferson Moraes Teixera <smoraes-@cbr9s6.42barcelona.com>
2023-12-10 14:30:23 +0100

    syncgit

--- syncgit.sh
+++ syncgit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rsync -av --progress ~/Templates/Repos/* ~/git/42Piscine --exclude .git
-
 cd ~/git/42Piscine/
 
+rsync -av --progress ~/Templates/Repos/* ~/git/42Piscine --exclude .git
+
+git add .
 echo "[+] Pushing to 42Piscine"
 echo "[+] Commit: "
 read -p "Introduce commit: " coomit
-git add .
 git commit -m $coomit
 git push
 
