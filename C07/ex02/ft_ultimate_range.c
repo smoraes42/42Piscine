44ce7a228891a902bc2ce5f71e0c347d8d7f8c81
Stefferson Moraes Teixera <smoraes-@cbr9s6.42barcelona.com>
2023-12-12 22:21:26 +0100

    C07

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -6,14 +6,51 @@
 /*   By: smoraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 18:21:17 by smoraes-          #+#    #+#             */
-/*   Updated: 2023/12/11 18:28:14 by smoraes-         ###   ########.fr       */
+/*   Updated: 2023/12/12 22:19:48 by smoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
+{
+	int	nrng;
+	int	*result;
+
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	nrng = max - min;
+	result = malloc(sizeof(int) * (nrng));
+	if (result == NULL)
+	{
+		*range = NULL;
+		return (-1);
+	}
+	*range = result;
+	nrng = 0;
+	while (max > min)
+	{
+		result[nrng] = min;
+		min++;
+		nrng++;
+	}
+	for (int j = 0; j < max; j++)
+	{
+		printf("%d ",*range[j]);
+	}
+	return (nrng);
+
+}
 
 int main(void)
 {
-
+	int arr[20];
+	int z = ft_ultimate_range(arr, 3, 20);
+	printf("%d", z);
 	return (0);
 }
