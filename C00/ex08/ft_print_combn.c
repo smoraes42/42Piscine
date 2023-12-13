8cf043b6b71f3918fb4dcc60255e522f24e65df9
Stefferson Moraes Teixera <smoraes-@cbr11s3.42barcelona.com>
2023-12-13 21:55:51 +0100

    TODO_fix_ex08

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -6,7 +6,7 @@
 /*   By: smoraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 08:59:49 by smoraes-          #+#    #+#             */
-/*   Updated: 2023/12/06 09:23:56 by smoraes-         ###   ########.fr       */
+/*   Updated: 2023/12/13 21:54:58 by smoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_putchar(char c)
 {
-	write(1, &c,1);
+	write(1, &c, 1);
 }
 
 void	print_array(char *v, int size, int last)
 {
 	int		i;
-	
+
 	i = 0;
 	if (v[0] == last)
 	{
@@ -31,7 +31,7 @@ void	print_array(char *v, int size, int last)
 	}
 	while (i < size)
 		ft_putchar(v[i++]);
-	write(1,",",2);
+	write(1, ",", 2);
 }
 
 void	ft_print_combn(int n)
@@ -43,26 +43,25 @@ void	ft_print_combn(int n)
 	int		it;
 
 	it = 0;
-	if (n <= 0 || n > 10)
-	{
-		write(1, "Insert a number between 0 and 10.\n>",34);
+	if (n < 0 || n > 10)
 		return ;
-	}
 	while (it < n)
 	{
-		v[it] = i + 48;
-		v_max[it] = (10 - n) + i + 48;
+		v[it] = it + 48;
+		v_max[it] = (10 - n) + it + 48;
 	}
 	print_array(v, n, v_max[0]);
 	while (v[0] != v_max[0])
 	{
-		flag = n-1;
+		flag = n - 1;
 		while (v[flag] == v_max[flag])
 			--flag;
 		base = ++(v[flag]);
+	}
 }
 int main(void)
 {
+	ft_print_combn(9);
 
 	return (0);
 }
