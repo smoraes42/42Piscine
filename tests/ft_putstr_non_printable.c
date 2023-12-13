c2548e93d2f1ae600d28a697c4024f779246a2b9
Stefferson Moraes Teixera <smoraes-@cbr11s3.42barcelona.com>
2023-12-13 21:40:54 +0100

    putstr

--- tests/ft_putstr_non_printable.c
+++ tests/ft_putstr_non_printable.c
@@ -6,45 +6,64 @@
 /*   By: smoraes- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 01:55:33 by smoraes-          #+#    #+#             */
-/*   Updated: 2023/12/13 21:40:32 by smoraes-         ###   ########.fr       */
+/*   Updated: 2023/12/13 21:21:33 by smoraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_convert_hexa(unsigned char c)
-{
-	char	*hexabase;
-
-	hexabase = "0123456789abcdef";
-	if (c / 16 > 0)
-	{
-		ft_putchar(hexabase[c / 16]);
-		ft_putchar(hexabase[c % 16]);
-	}
-	else
-	{
-		ft_putchar('0');
-		ft_putchar(hexabase[c]);
-	}
-}
+//static void	print_hex(int np)
+//{
+//	char	*hex;
+//
+//	hex = "0123456789abcdef";
+//	if (np > 16)
+//	{
+//		print_hex(np / 10);
+//		print_hex(np % 10);
+//	}
+//	else
+//		write(1, &hex[np], 1);
+//}
+//
+//void	ft_putstr_non_printable(char *str)
+//{
+//	int	i;
+//
+//	i = 0;
+//	while (str[i])
+//	{
+//		if ((str[i] >= 0 && str[i] <= 31) || str[i] == 127)
+//		{
+//			write(1, "\\", 1);
+//			if (str[i] < 16)
+//				write(1, "0", 1);
+//			print_hex(str[i]);
+//		}
+//		else
+//			write(1, &str[i], 1);
+//		i += 1;
+//	}
+//}
 
 void	ft_putstr_non_printable(char *str)
 {
-	unsigned int	i;
+	int		a;
+	int		b;
+	int		i;
+	char		*hxa;
 
 	i = 0;
-	while (str[i] != '\0')
+	hxa = "0123456789abcdef";
+	while (str[i])
 	{
-		if ((str[i] < 32) || str[i] > 126)
+		if ((str[i] <= 31) || str[i] <= 126)
 		{
 			write(1, "\\", 1);
-			ft_convert_hexa(str[i]);
+			a = (str[i] / 16);
+			b = (str[i] % 16);
+			write(1, &hxa[a], 1);
+			write(1, &hxa[b], 1);
 		}
 		else
 		{
@@ -53,3 +72,15 @@ void	ft_putstr_non_printable(char *str)
 		i++;
 	}
 }
+/*
+int main(void)
+{
+	//char *str = "This is pretty weird\n\t.";
+
+	char v[2] = "";
+	ft_putstr_non_printable(v);
+
+
+	return (0);
+}
+*/
